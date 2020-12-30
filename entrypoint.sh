c110c607917021b10d2212ef422dd8dafddffb09
dark0ghost <dark0ghostworkmail@seznam.cz>
2020-12-30 11:52:30 +0300

    .

--- entrypoint.sh
+++ entrypoint.sh
@@ -1,14 +1,10 @@
 #!/bin/sh -l
 
-# shellcheck disable=SC2034
-# shellcheck disable=SC1073
-# shellcheck disable=SC2154
-if "$INPUT_LINK" != "1"; then
-  arduino-cli  core update-index --additional-urls $INPUT_LINK
-fi
-# shellcheck disable=SC2164
-cd "$GITHUB_WORKSPACE"
-for i in $INPUT_FILE; do
+arduino-cli  core update-index --additional-urls $INPUT_LINK
+
+cd $GITHUB_WORKSPACE
+
+for i in $INPUT_FILE ; do
   arduino-cli compile i
 done
-  exit 1
+exit 1
